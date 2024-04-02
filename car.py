ef32882f11bf51cfa490e7573697a238b21b7db2
Sensei Orwa <146319656+Sensei-Orwa@users.noreply.github.com>
2024-04-02 10:43:48 +0300

    Add files via upload

---
+++ car.py
@@ -0,0 +1,12 @@
+class Car:
+
+    def __init__(self, make, model, color, year):
+        self.make = make
+        self.model = model
+        self.color = color
+        self.year = year
+    
+    def drive(self):
+        print("This car is driving")
+    def  stop(self):
+        print("This car will stop")
---
+++ main.py
@@ -0,0 +1,4 @@
+from car import Car
+
+car1 = Car("Chevy", "Covette", 2021, "blue")
+print(car1.make)
